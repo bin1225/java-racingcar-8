2f82fd966c9fc1d309287809bb73532febbfa8e6
kkb <gimgyubin1225@gmail.com>
2025-10-25 21:53:29 +0900

    fix(domain): Random 값 추출 메서드를 pickNumberInRanger로 변경
    - 프로그래밍 요구 사항 2에 따라 Randoms클래스의 pickNumberInRange 메서드를 사용한다.

--- src/main/java/racingcar/domain/CarMover.java
+++ src/main/java/racingcar/domain/CarMover.java
@@ -1,15 +1,14 @@
 package racingcar.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.List;
-import java.util.stream.IntStream;
 
 public class CarMover {
 
-    private final List<Integer> candidateNumbers = IntStream.rangeClosed(0, 9).boxed().toList();
+    private static final int NUMBER_START = 0;
+    private static final int NUMBER_END = 9;
 
     public void moveIfOverThreshold(RacingCar car, int threshold) {
-        int pickNumber = Randoms.pickNumberInList(candidateNumbers);
+        int pickNumber = Randoms.pickNumberInRange(NUMBER_START,NUMBER_END);
         if (pickNumber >= threshold) {
             car.moveForward();
         }
