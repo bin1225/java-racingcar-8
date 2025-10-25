9bb0cf591125e3031531a01de5d9825143d995cd
kkb <gimgyubin1225@gmail.com>
2025-10-25 22:08:56 +0900

    feat(main): 자동차 경주 게임 실행 및 입/출력 흐름 구현
    - RacingManager, CarMover, WinnerDeterminer 클래스를 활용하여 경주 게임 진행
    - InputView, OutputView 를 활용하여 입/출력 흐름 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,36 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.domain.CarMover;
+import racingcar.domain.RacingCar;
+import racingcar.domain.RacingManager;
+import racingcar.domain.WinnerDeterminer;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
+
+    private static final int MOVE_THRESHOLD = 4;
+
+    private static final RacingManager racingManager = new RacingManager();
+    private static final CarMover carMover = new CarMover();
+    private static final WinnerDeterminer winnerDeterminer = new WinnerDeterminer();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> carNames = InputView.getCarNames();
+        int attemptCount = InputView.getAttemptCount();
+
+        List<RacingCar> participants = racingManager.registerCars(carNames);
+
+        OutputView.printResultMessage();
+        while (attemptCount-- > 0) {
+            for (RacingCar r : participants) {
+                carMover.moveIfOverThreshold(r, MOVE_THRESHOLD);
+            }
+            OutputView.printCurrentProgress(participants);
+        }
+
+        List<RacingCar> winners = winnerDeterminer.getWinners(participants);
+        OutputView.printWinner(winners);
     }
 }
